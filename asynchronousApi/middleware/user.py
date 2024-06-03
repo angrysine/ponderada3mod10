f8491211515e59220c407f781172a3b776579b9d
angrysine <betorm20022@gmail.com>
2024-06-02 22:09:16 -0300

    fix: fixed error in auth verification

--- asynchronousApi/middleware/user.py
+++ asynchronousApi/middleware/user.py
@@ -9,6 +9,6 @@ def getUser(token: Annotated[str, Depends(oauth2_scheme)]):
     print(token)
     response = requests.get("http://login:5002/getUser",headers = {"Authorization": "Bearer "+token})
     if response.status_code != 200:
-        raise HTTPException(response.status_code, response.json)
+        return HTTPException(response.status_code, response.json)
     json = response.json()
     return json
